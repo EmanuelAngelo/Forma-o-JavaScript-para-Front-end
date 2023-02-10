a905dc66054cfe80df6dc75365015811507d916b
Emanuel Angelo <emanuelangelo@outlook.com.br>
2023-02-10 13:29:45 -0300

    att

--- javascript-manipulando-dom/js/main.js
+++ javascript-manipulando-dom/js/main.js
@@ -1,12 +1,17 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", () => {
-    console.log("cliquei no robo")
-})
+const controle = document.querySelectorAll(".controle-ajuste")
 
-function dizOi(nome){
-    console.log("oi", nome)
-    console.log("Bem vindo ao Robotron 2000")
+somar.addEventListener("click", ()=>{manipulaDados("somar")})
+subtrair.addEventListener("click", ()=>{manipulaDados("subtrair")})
+
+
+function manipulaDados(operacao){
+    if(operacao === "subtrair") {
+        braco.value = parseInt(braco.value) -1
+    } else {
+        braco.value = parseInt(braco.value) +1
+    }
 }
-
-dizOi("pedro")
