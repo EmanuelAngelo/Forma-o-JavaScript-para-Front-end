e9cbc3881429b29d7e3018e48d7e54349295cc54
Emanuel Angelo <emanuelangelo@outlook.com.br>
2023-02-10 16:21:25 -0300

    usando data-attribute

--- javascript-manipulando-dom/js/main.js
+++ javascript-manipulando-dom/js/main.js
@@ -1,17 +1,17 @@
-const subtrair = document.querySelector("#subtrair")
-const somar = document.querySelector("#somar")
-const braco = document.querySelector("#braco")
+const controle = document.querySelectorAll("[data-controle]")
 
-const controle = document.querySelectorAll(".controle-ajuste")
+controle.forEach((elemento)=>{
+    elemento.addEventListener("click",(evento)=> {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+    })
+})
 
-somar.addEventListener("click", ()=>{manipulaDados("somar")})
-subtrair.addEventListener("click", ()=>{manipulaDados("subtrair")})
+function manipulaDados(operacao, controle){
+    const peca = controle.querySelector("[data-contador]")
 
-
-function manipulaDados(operacao){
-    if(operacao === "subtrair") {
-        braco.value = parseInt(braco.value) -1
+    if(operacao === "-") {
+        peca.value = parseInt(peca.value) -1
     } else {
-        braco.value = parseInt(braco.value) +1
+        peca.value = parseInt(peca.value) +1
     }
 }
