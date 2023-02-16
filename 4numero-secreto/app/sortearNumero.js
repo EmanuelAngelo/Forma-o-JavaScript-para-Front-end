fd35371f152378f18545dc13db26174116c79bb0
Emanuel Angelo <mario.mhsouza@gmail.com>
2023-02-16 16:20:13 -0300

    validações das falas

--- 4numero-secreto/app/sortearNumero.js
+++ 4numero-secreto/app/sortearNumero.js
@@ -2,7 +2,7 @@ const menorValor = 1
 const maiorValor = 1000
 const numeroSecreto = gerarNumeroAleatorio()
 
-function gerarNumeroAleatorio(){
+function gerarNumeroAleatorio() {
     return parseInt(Math.random() * maiorValor + 1)
 }
 
