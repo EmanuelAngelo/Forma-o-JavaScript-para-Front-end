bcbc68917c8adf7a4e2c1bb8d5191b6d5ca85343
Mario Souza <mario.mhsouza@gmail.com>
2023-02-23 09:04:53 -0300

    Async Await

--- 5javascript-consumindo-tratando-dados-api/script.js
+++ 5javascript-consumindo-tratando-dados-api/script.js
@@ -1,7 +1,17 @@
-var consultaCep = fetch('https:\\viacep.com.br/ws/65058139/json/')
-.then(resposta => resposta.json())
-.then(r => {
-        if (r.erro){
-            throw Error('Esse cep nao existe')
-        } else console.log(r)
-    }).catch(erro => console.log(erro));
+async function buscaEndereco(){
+    var consultaCep = await fetch('https:\\viacep.com.br/ws/65058139/json/');
+    var consultaCEPConvertida = await consultaCep.json();
+    console.log(consultaCEPConvertida);
+}
+
+buscaEndereco();
+
+
+// .then(resposta => resposta.json())
+// .then(r => {
+//         if (r.erro){
+//             throw Error('Esse cep nao existe')
+//         } else console.log(r)
+//     })
+//     .catch(erro => console.log(erro))
+//     .finally(mensagem => console.log('processamento concluido'));
