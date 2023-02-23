1551b77099b22f144c56dfa2c6fde71a2332953d
Emanuel Angelo <mario.mhsouza@gmail.com>
2023-02-23 09:32:36 -0300

    Promisse All

--- 5javascript-consumindo-tratando-dados-api/script.js
+++ 5javascript-consumindo-tratando-dados-api/script.js
@@ -1,17 +1,22 @@
-async function buscaEndereco(){
+async function buscaEndereco(cep){
     try {
-        var consultaCep = await fetch('https:\\viacep.com.br/ws/01001250/json/');
+        var consultaCep = await fetch(`https:\\viacep.com.br/ws/${cep}/json/`);
         var consultaCEPConvertida = await consultaCep.json();
         if (consultaCEPConvertida.erro){
             throw Error('CEp nao achado.')
         }
         console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
         } catch (erro) {
             console.log(erro)
     }
 }
 
-buscaEndereco();
+
+// let ceps = ['65058139', '65058136'];
+// let conjuuntosCeps = ceps.map(valores => buscaEndereco(valores));
+// console.log(conjuuntosCeps)
+// Promise.all(conjuuntosCeps).then(respostas => console.log(respostas));
 
 
 // .then(resposta => resposta.json())
