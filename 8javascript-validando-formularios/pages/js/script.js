9edeea2dee94abd0b11a972841fb9b7f30ddd958
Emanuel Angelo <emanuelangelo@outlook.com.br>
2023-03-06 11:19:39 -0300

    datas em js, comparacao para verificar datas maioridade

--- 8javascript-validando-formularios/pages/js/script.js
+++ 8javascript-validando-formularios/pages/js/script.js
@@ -1,4 +1,6 @@
 import ehUmCPF from "./valida-cpf";
+import ehMaiorDeIdade from "./valida-idade";
+
 const camposDoFormulario = document.querySelectorAll("[required]");
 camposDoFormulario.forEach((campo) => {
     campo.addEventListener("blur", () => verificaCampo(campo));
@@ -7,5 +9,8 @@ camposDoFormulario.forEach((campo) => {
 function verificaCampo(campo) {
     if (campo.name == "cpf" && campo.value.length >= 11) {
         ehUmCPF(campo);
+    }
+    if (campo.name == "aniversario" && campo.value != "") {
+        ehMaiorDeIdade(campo);
     }
 }
